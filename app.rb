168cf5dd948751f4ecb8d11db3a401c07972c0d9
Todd Moore <todd@toddm.me>
2012-03-20 10:51:56 +1100

    Initial Commit.

---
+++ app.rb
@@ -0,0 +1,11 @@
+require "sinatra"
+
+set :public_folder, File.dirname(__FILE__) + '/assets'
+
+get '/' do
+ 	send_file 'index.html'
+end
+
+use Rack::Auth::Basic, "Restricted Area" do |username, password|
+  [username, password] == ['admin', 'Password1dt']
+end
